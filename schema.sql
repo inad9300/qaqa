1aa36d5d2f4a8b141f2b02c11df34d4ab88099a0
inad9300 <inad9300@gmail.com>
2019-07-07 17:46:43 +0200

    Add initial database schema. Adapt readme file.

---
+++ schema.sql
@@ -0,0 +1,23 @@
+drop database if exists qaqadb;
+drop user if exists 'qaqaroot'@'localhost';
+
+create database qaqadb;
+create user 'qaqaroot'@'localhost' identified by 'qaqapass';
+grant select, insert on qaqadb.* to 'qaqaroot'@'localhost';
+
+use qaqadb;
+
+create table questions (
+    id int unsigned auto_increment primary key,
+    title varchar(100) not null,
+    content varchar(500) not null
+);
+
+create table answers (
+    id int unsigned auto_increment primary key,
+    question_id int unsigned,
+    content varchar(500) not null,
+    time timestamp,
+
+    foreign key (question_id) references questions(id)
+);
